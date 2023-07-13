a0d7e9dfa51b11bd665b38f0e3324c6e5e1b5a23
Ephrem Tadesse <ephremtadesse001@gmail.com>
2023-07-14 01:08:55 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,10 @@
  *
  * Return: Alway 1 (Success)
  */
- int main() 
+ int main(void)
 {
-	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	size_t len = strlen(message);
 	write(1, message, len);
-	return 1;
+	return (1);
 }

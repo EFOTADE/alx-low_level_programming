922aeeaf9b02ad3d2f3df11c3f213e07fca1f5fe
Ephrem Tadesse <ephremtadesse001@gmail.com>
2023-07-13 13:03:29 -0700

    101-quote.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#!/bin/bash
+#include<stdio.h>
+
+int main(void)
+{
+        char a;
+        int b;
+        long int c;
+        long long int d;
+        float e;
+
+        printf("Size of char: %ld bytes(s)\n", (unsigned long)sizeof(a));
+        printf("Size of int: %ld bytes(s)\n", (unsigned long)sizeof(b));
+        printf("Size of long int: %ld bytes (s)\n", (unsigned long)sizeof(c));
+        printf("Size of long long int: %ld bytes(s)\n", (unsigned long)sizeof(d));
+        printf("Size of float: %ld bytes(s)\n", (unsigned long)sizeof(e));
+        return(0);
+}

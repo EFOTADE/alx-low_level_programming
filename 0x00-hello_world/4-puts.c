29060ec0f3464ea9cdedb7696881dd795b261e75
Ephrem Tadesse <ephremtadesse001@gmail.com>
2023-07-13 13:42:17 -0700

    101-quote.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return! always 0 (Success)
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle \n");
+
 	return(0);
 }

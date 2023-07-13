ff6ba790ac94d399e6c4829a13109ecd1d337c38
Ephrem Tadesse <ephremtadesse001@gmail.com>
2023-07-14 00:58:41 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <unistd.h>
 #include <string.h>
-
-int main() {
-
-const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-size_t len = strlen(message);
-
-write(1, message, len);
-
-return 1;
-
+/**
+ * main - Entry point
+ *
+ * Return: Alway 1 (Success)
+int main() 
+{
+	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t len = strlen(message);
+	write(1, message, len);
+	return 1;
 }

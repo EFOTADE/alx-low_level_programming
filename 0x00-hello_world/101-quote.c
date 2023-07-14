cb013e81f486e2f56f50157f2e5940f5078517b7
Ephrem Tadesse <ephremtadesse001@gmail.com>
2023-07-14 18:17:35 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-size_t len = strlen(message);
-write(1, message, len);
+const char *m ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+size_t len = strlen(m);
+write(1, m, len);
 return (1);
 }
 

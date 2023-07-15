131d84d9559857228d42ac742a66203cc28ded2f
Ephrem Tadesse <ephremtadesse001@gmail.com>
2023-07-15 09:00:43 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,30 +4,30 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
-{
-  int a;
-  int b;
-  for (a=0; a<10; a++)
-  {
-      for (b=a; b<10; b++)
-      {
-	  if(a!=b)
-	  {
-	  	 if ( a == 8 && b == 9 )
+int main(void)
+{ 
+        int a;
+        int b;
+        for (a=0; a<10; a++)
+        {
+                for (b=a; b<10; b++)
                 {
-                	 printf("%d%d", a,b);
+                        if(a!=b)
+                        {
+                                if ( a == 8 && b == 9 )
+                                {
+                                         printf("%d%d ", a,b);
+                                 }
+                                else    
+                                {
+                                         printf("%d%d, ", a,b);
+                                }
+                        }
+                         else
+                        {
                 }
-                else
-                {
-               		 printf("%d%d, ", a,b);
                 }
-	  }
-          else
-          {
-          }
-	}
-  }
+        }
 
-    return (0);
+        return (0);
 }

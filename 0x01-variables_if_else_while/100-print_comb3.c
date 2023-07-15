9681324f91ab76fefdec50bf613b42ed002bc535
Ephrem Tadesse <ephremtadesse001@gmail.com>
2023-07-15 10:39:48 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - prints all possible different combination of all digits
  *
@@ -7,24 +6,25 @@
  */
 int main(void)
 {
-    int a;
-    int b;
-    for (a = 0; a <= 8; a++)
-    {
-        for (b = a + 1; b <= 9; b++)
-        {
-            putchar((a + '0'));
-            if ((a != 1) || (b != 0))
-            {
-            putchar((b + '0'));
-                if (a < 8)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+int a;
+int b;
+for (a = 0; a <= 8; a++)
+{
+for (b = a + 1; b <= 9; b++)
+{
+putchar((a + '0'));
+if ((a != 1) || (b != 0))
+{
+putchar((b + '0'));
+if (a < 8)
+{
+putchar(',');
+putchar(' ');
 }
+}
+}
+}
+putchar('\n');
+return (0);
+}
+

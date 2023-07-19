202d8a663c2d496475289e5844d10fedc1fca560
Ephrem Tadesse <ephremtadesse001@gmail.com>
2023-07-18 17:20:12 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,11 +11,11 @@ int fib[50];
 int i;
 fib[0] = 1;
 fib[1] = 2;
-for(i = 2; i < 50; i++)
+for (i = 2; i < 50; i++)
 {
-	fib[i] = fib[i-1] + fib[i-2];
+	fib[i] = fib[i - 1] + fib[i - 2];
 }
-for(i = 0; i < 49; i++)
+for (i = 0; i < 49; i++)
 {
 	printf("%d, ", fib[i]);
 }

d7f342f45fbb5d4260bde10026738e73bdd27ab8
Ephrem Tadesse <ephremtadesse001@gmail.com>
2023-07-24 20:37:33 +0300

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that take a pointer to an int a parameter
+ * and updates the value it points to 98
+ * @n: input
+ * Return: n
+ */
+int _strlen(char *s);
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}

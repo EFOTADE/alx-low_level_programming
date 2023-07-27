7848a3573d29e563234dad8a6d859d71e267c3b7
Ephrem Tadesse <ephremtadesse001@gmail.com>
2023-07-27 17:15:05 +0300

    6-cap_string.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,15 +1,14 @@
 #include "main.h"
-
 /**
  * string_toupper - change all lowercase to uppercase
  * @n: pointer
  *
  * Return: n
  */
-
 char *string_toupper(char *n)
 {
 	int i;
+
 	i = 0;
 	while (n[i] != '\0')
 	{
@@ -19,3 +18,4 @@ char *string_toupper(char *n)
 	}
 	return (n);
 }
+
